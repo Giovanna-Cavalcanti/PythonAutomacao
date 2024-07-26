ba6708765e5ad6f77d163f171a587f2904de4d36
gio-ferreira <giovanna.ferreira2022@gmail.com>
2024-07-25 21:31:48 -0300

    changing the tipe of files to help later

--- testeideia.py
+++ testeideia.py
@@ -1,19 +1,31 @@
 import pandas as pd
 import os
+import json
+
+def ler_arquivo_codigos(caminho_arquivo):
+    with open(caminho_arquivo, 'r') as arquivo:
+        linhas = arquivo.readlines()
+    return [linha.strip() for linha in linhas]
 
 pasta_planilhas = r'C:\Users\giova\OneDrive\Área de Trabalho\Python testes e estudos\planilhasxlsx'
 pasta_pep = r'C:\Users\giova\OneDrive\Área de Trabalho\Python testes e estudos\pepLer'
-arquivo_output = r'C:\Users\giova\OneDrive\Área de Trabalho\Python testes e estudos\output.txt'
+nomes = r'C:\Users\giova\OneDrive\Área de Trabalho\Python testes e estudos\nomes.txt'
 
 peps = os.listdir(pasta_pep)
-arquivos = os.listdir(pasta_planilhas)
+arquivos_planilhas = os.listdir(pasta_planilhas)
 
-arquivo_desejado = arquivos[0]
 pep_desejado = os.path.join(pasta_pep, peps[0])
+arquivo_desejado = arquivos_planilhas[0]
+
+def save_to_json(data, filename):
+    with open(filename, 'w') as json_file:
+        json.dump(data, json_file, indent=4)
+        
 
 df =  pd.read_excel(os.path.join(pasta_planilhas, arquivo_desejado))
 
-# print(pep_desejado)
+coisa = []
+
 
 for index, row in df.iterrows():
         if row.iloc[2] == "Dermonecrotic":
@@ -25,8 +37,14 @@ for index, row in df.iterrows():
 
             if inicio != -1 and fim != -1:
                 substring = conteudo[inicio:fim]
-                with open(arquivo_output, 'a')as file: 
-                    file.write(substring)    
+                coisa.append({
+                    "conteudo": substring
+                })
+                # with open(arquivo_output, 'a')as file: 
+                #     file.write(substring)
+    
+        
+save_to_json(coisa, ler_arquivo_codigos(nomes)[0] + ".json")
 
 
 
